d09b443383958da5c78062aaa1d431af165ca6ea
madhawesh rai <madhawesh.rai@gmail.com>
2021-05-19 13:17:07 +0530

    secont TC commit v2

--- src/test/java/TestCases/TestCase2.java
+++ src/test/java/TestCases/TestCase2.java
@@ -21,9 +21,13 @@ public void guruTEstcase1()
 	obj.mobile_click();
 	System.out.println("mobile clicked");
 	TestCase2_Element obj2 = new TestCase2_Element(wdriver);
+	//Getting main page sony value
 	obj2.sony_value1();
+	//click on sony icon
 	obj2.sony_click();
+	//compare both prices
 	obj2.price_compare();
+	
 	System.out.println("TC2 passed");
 	
 	
